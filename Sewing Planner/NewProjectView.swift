fac170e849279b0996c24658c293be5cc9884602
Lloyd Massiah <artmis9@protonmail.com>
2024-08-04 17:51:47 -0400

    updating new project view to have one alert after learning there can't be more than one alert modifier
    - adding tests that test the custom back button on the happy path
    - the tests are to check if user wants to save after making modifications to a project

--- Sewing Planner/NewProjectView.swift
+++ Sewing Planner/NewProjectView.swift
@@ -9,177 +9,158 @@ import GRDB
 import SwiftUI
 
 struct NewProjectView: View {
-    // used for dismissing a view(basically the back button)
-    @Environment(\.dismiss) private var dismiss
-    @Environment(\.appDatabase) private var appDatabase
-    @State var clicked = true
-    var projectId: Int64 = 0
-    @State var project = Project()
-    @State var name = ""
-    @State var newStep = ""
-    @State var projectSteps: [ProjectStepData] = [ProjectStepData]()
-    @State var showAddTextboxPopup = false
-    @State var isAddingInstruction = false
-    @State var doesProjectHaveName = false
-    @State var showAlertIfProjectNotSaved = false
-    @Binding var projectsNavigation: [Project]
-    
-    
-    
-    private var isNewStepValid: Bool {
-        newStep.trimmingCharacters(in: .whitespaces).isEmpty
-    }
-    
-    private var isProjectValid: Bool {
-        !project.name.trimmingCharacters(in: .whitespaces).isEmpty
-    }
-    
-    private var isNewProjectEmpty: Bool {
-        project.name.trimmingCharacters(in: .whitespaces).isEmpty && projectSteps.isEmpty
-    }
-    
-    var body: some View {
-        VStack {
-            ProjectName(project: $project)
-            ProjectStepsView(projectSteps: self.$projectSteps)
-            if isAddingInstruction {
-                HStack {
-                    TextField("write your instruction", text: $newStep).onSubmit {
-                        // add a popup telling user that instruction can't be empty
-                        guard !newStep.isEmpty else { return }
-                        projectSteps.append(ProjectStepData(text: newStep, isEditing: false, isComplete: false))
-                        newStep = ""
-                        isAddingInstruction = false
-                    }.textFieldStyle(.plain)
-                        .accessibilityIdentifier("NewStepTextField")
-                    
-                    Button("Cancel") {
-                        isAddingInstruction = false
-                        newStep = ""
-                    }
-                    Button("Add") {
-                        // add a popup telling user that instruction can't be empty
-                        // guard !newStep.trimmingCharacters(in: .whitespaces).isEmpty else { return }
-                        guard !isNewStepValid else { return }
-                        
-                        do {
-                            try appDatabase.addProjectStep(text: newStep, projectId: project.id)
-                        } catch {
-                            fatalError("error when inserting step: \(newStep) for project id: \(project.id)\n\n\(error)")
-                        }
-                        
-                        projectSteps.append(ProjectStepData(text: newStep, isEditing: false, isComplete: false))
-                        newStep = ""
-                        isAddingInstruction = false
-                        
-                    }.disabled(isNewStepValid)
-                        .accessibilityIdentifier("AddNewStepButton")
-                }
-            }
-            Form {
-                Button("New Step") {
-                    isAddingInstruction = true
-                }.accessibilityIdentifier("NewStepButton")
-                Button("Save") {
-                    guard isProjectValid else {
-                        showAlertIfProjectNotSaved = true
-                        return }
-                    
-                    let projectId = try! appDatabase.addProject(name: "")
-                    project = Project(name: "", id: projectId)
-                    print("creating project with id: \(projectId)")
-                    // add alert that says they need to pass validation
-                    print(project.name)
-                    print(projectSteps.count)
-                    
-                    do {
-                        for step in projectSteps {
-                            try appDatabase.addProjectStep(text: step.text, projectId: project.id)
-                        }
-                    } catch {
-                        fatalError("error when inserting step: \(newStep) for project id: \(project.id)\n\n\(error)")
-                    }
-                    
-                    
-                    projectsNavigation.removeLast()
-                    
-                }.accessibilityIdentifier("SaveButton")
-                    .alert("Unsaved Changes", isPresented: $showAlertIfProjectNotSaved) {
-                        HStack {
-                            TextField("Enter a project name", text: $project.name)
-                            Button("Save") {
-                                let projectId = try! appDatabase.addProject(name: project.name)
-                                project = Project(name: project.name, id: projectId)
-                                print("creating project with id: \(projectId)")
-                                
-                                do {
-                                    for step in projectSteps {
-                                        try appDatabase.addProjectStep(text: step.text, projectId: project.id)
-                                    }
-                                } catch {
-                                    fatalError("error when inserting step: \(newStep) for project id: \(project.id)\n\n\(error)")
-                                }
-                                
-                                dismiss()
-                            }
-                            Button(role: .destructive) {
-                                dismiss()
-                            } label: {
-                                Text("Discard")
-                            }
-                            
-                        }
-                    } message: {
-                        Text("Enter a name to save this project.")
-                    }
-                
-            }
+  // used for dismissing a view(basically the back button)
+  @Environment(\.dismiss) private var dismiss
+  @Environment(\.appDatabase) private var appDatabase
+  @State var clicked = true
+  var projectId: Int64 = 0
+  @State var project = Project()
+  @State var name = ""
+  @State var newStep = ""
+  @State var projectSteps: [ProjectStepData] = [ProjectStepData]()
+  @State var showAddTextboxPopup = false
+  @State var isAddingInstruction = false
+  @State var doesProjectHaveName = false
+  @State var showAlertIfProjectNotSaved = false
+  @Binding var projectsNavigation: [Project]
+
+  private var isNewStepValid: Bool {
+    newStep.trimmingCharacters(in: .whitespaces).isEmpty
+  }
+
+  private var isProjectValid: Bool {
+    !project.name.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+  }
+
+  private var isNewProjectEmpty: Bool {
+    project.name.trimmingCharacters(in: .whitespaces).isEmpty && projectSteps.isEmpty
+  }
+
+  var body: some View {
+    VStack {
+      ProjectName(project: $project)
+      ProjectStepsView(projectSteps: self.$projectSteps)
+      if isAddingInstruction {
+        HStack {
+          TextField("write your instruction", text: $newStep).onSubmit {
+            // add a popup telling user that instruction can't be empty
+            guard !newStep.isEmpty else { return }
+            projectSteps.append(ProjectStepData(text: newStep, isEditing: false, isComplete: false))
+            newStep = ""
+            isAddingInstruction = false
+          }.textFieldStyle(.plain)
+            .accessibilityIdentifier("NewStepTextField")
+
+          Button("Cancel") {
+            isAddingInstruction = false
+            newStep = ""
+          }
+          Button("Add") {
+            // add a popup telling user that instruction can't be empty
+            // guard !newStep.trimmingCharacters(in: .whitespaces).isEmpty else { return }
+            guard !isNewStepValid else { return }
+
+            // think about what to do here for validation or something
+
+            projectSteps.append(ProjectStepData(text: newStep, isEditing: false, isComplete: false))
+            newStep = ""
+            isAddingInstruction = false
+
+          }.disabled(isNewStepValid)
+            .accessibilityIdentifier("AddNewStepButton")
         }
-        .navigationBarBackButtonHidden(true).toolbar {
-            ToolbarItem(placement: .navigation) {
-                Button {
-                    if isNewProjectEmpty {
-                        dismiss()
-                        return
-                    }
-                    
-                    showAlertIfProjectNotSaved = true
-                } label: {
-                    HStack(alignment: VerticalAlignment.center) {
-                        Image(systemName: "arrowshape.backward.fill")
-                        Text("Back")
-                    }
-                }
-                .alert("Unsaved Changes", isPresented: $showAlertIfProjectNotSaved) {
-                    VStack {
-                        
-                        Button(role: .destructive) {
-                            // no need to do anything as changes haven't been saved yet
-                            dismiss()
-                        } label: {
-                            Text("Discard")
-                        }
-                        Button("Save") {
-                            let projectId = try! appDatabase.addProject(name: project.name)
-                            project = Project(name: project.name, id: projectId)
-                            print("creating project with id: \(projectId)")
-                            
-                            do {
-                                for step in projectSteps {
-                                    try appDatabase.addProjectStep(text: step.text, projectId: project.id)
-                                }
-                            } catch {
-                                fatalError("error when inserting step: \(newStep) for project id: \(project.id)\n\n\(error)")
-                            }
-                            
-                            dismiss()
-                        }.keyboardShortcut(/*@START_MENU_TOKEN@*/.defaultAction/*@END_MENU_TOKEN@*/)
-                    }
-                } message: {
-                    Text("Do you want to save this project?")
-                }
+      }
+      Form {
+        Button("New Step") {
+          isAddingInstruction = true
+        }.accessibilityIdentifier("NewStepButton")
+        Button("Save") {
+          guard isProjectValid else {
+            showAlertIfProjectNotSaved = true
+            return
+          }
+
+          let projectId = try! appDatabase.addProject(name: "")
+          project = Project(name: "", id: projectId)
+          print("creating project with id: \(projectId)")
+          // add alert that says they need to pass validation
+          print(project.name)
+          print(projectSteps.count)
+
+          do {
+            for step in projectSteps {
+              try appDatabase.addProjectStep(text: step.text, projectId: project.id)
             }
-        }
-        
+          } catch {
+            fatalError(
+              "error when inserting step: \(newStep) for project id: \(project.id)\n\n\(error)")
+          }
+
+          projectsNavigation.removeLast()
+
+        }.accessibilityIdentifier("SaveButton")
+      }
     }
+    .navigationBarBackButtonHidden(true).toolbar {
+      ToolbarItem(placement: .navigation) {
+        BackButton {
+          if isNewProjectEmpty {
+            dismiss()
+            return
+          }
+
+          showAlertIfProjectNotSaved = true
+        }
+        .accessibilityIdentifier("ProjectViewCustomBackButton")
+        .alert("Unsaved Changes", isPresented: $showAlertIfProjectNotSaved) {
+          VStack {
+            if !isProjectValid {
+              TextField("Enter a project name", text: $project.name).accessibilityIdentifier(
+                "ProjectNameTextFieldInAlertUnsavedProject")
+
+            }
+            Button(role: .destructive) {
+              // no need to do anything as changes haven't been saved yet
+              dismiss()
+            } label: {
+              Text("Discard")
+            }
+            Button("Save") {
+              let projectId = try! appDatabase.addProject(name: project.name)
+              project = Project(name: project.name, id: projectId)
+              print("creating project with id: \(projectId)")
+
+              do {
+                for step in projectSteps {
+                  try appDatabase.addProjectStep(text: step.text, projectId: project.id)
+                }
+              } catch {
+                fatalError(
+                  "error when inserting step: \(newStep) for project id: \(project.id)\n\n\(error)")
+              }
+
+              dismiss()
+            }
+            .accessibilityIdentifier("SaveButtonInAlertUnsavedProject")
+            .keyboardShortcut( /*@START_MENU_TOKEN@*/.defaultAction /*@END_MENU_TOKEN@*/)
+          }
+        } message: {
+          Text("Do you want to save this project?")
+        }
+      }
+    }
+
+  }
+}
+
+struct BackButton: View {
+  let buttonAction: () -> Void
+  var body: some View {
+    Button(action: buttonAction) {
+      HStack(alignment: VerticalAlignment.center) {
+        Image(systemName: "arrowshape.backward.fill")
+        Text("Back")
+      }
+    }
+  }
 }
---
+++ Sewing PlannerUITests/NewProjectViewTest.swift
@@ -0,0 +1,128 @@
+//
+//  NewProjectViewTest.swift
+//  Sewing PlannerUITests
+//
+//  Created by Art on 8/2/24.
+//
+
+import XCTest
+
+final class NewProjectViewTest: XCTestCase {
+    var app: XCUIApplication!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
+        
+        let addNewProjectButton = app.buttons["AddNewProjectButton"]
+        XCTAssertTrue(addNewProjectButton.exists)
+        addNewProjectButton.click()
+        let wait = app.waitForExistence(timeout: TimeInterval(2))
+        print(wait)
+        
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testBackButtonForEmptyProject() throws {
+        let newStepButton = app.buttons["NewStepButton"]
+        XCTAssertTrue(newStepButton.exists)
+        
+        let projectNameTextfield = app.textFields["ProjectNameTextfield"]
+        XCTAssertTrue(projectNameTextfield.exists)
+        
+        // TODO: figure out why I need to search for the button twice, first in find toolbar buttons then the button I want in the toolbar
+        let backButton = app.toolbars.buttons["ProjectViewCustomBackButton"].buttons["ProjectViewCustomBackButton"]
+        XCTAssertTrue(backButton.exists)
+        backButton.click()
+        
+        let wait = app.waitForExistence(timeout: TimeInterval(1))
+        
+        let addNewProjectButton = app.buttons["AddNewProjectButton"]
+        XCTAssertTrue(addNewProjectButton.exists)
+    }
+    
+    func testBackButtonForProjectWithName() throws {
+        let newStepButton = app.buttons["NewStepButton"]
+        XCTAssertTrue(newStepButton.exists)
+        
+        let projectNameTextfield = app.textFields["ProjectNameTextfield"]
+        XCTAssertTrue(projectNameTextfield.exists)
+        projectNameTextfield.click()
+        projectNameTextfield.typeText("Project 1")
+        // figure out how to type a key without using a modifier
+        projectNameTextfield.typeKey(.enter, modifierFlags: .capsLock)
+        
+        // TODO: figure out why I need to search for the back button twice, first in find toolbar buttons then the button I want in the toolbar
+        let backButton = app.toolbars.buttons["ProjectViewCustomBackButton"].buttons["ProjectViewCustomBackButton"]
+        XCTAssertTrue(backButton.exists)
+        backButton.click()
+        
+        var wait = app.waitForExistence(timeout: TimeInterval(1))
+
+        let alertTextField = app.textFields["ProjectNameTextFieldInAlertUnsavedProject"]
+        XCTAssertFalse(alertTextField.exists)
+        
+        let saveButton = app.buttons["SaveButtonInAlertUnsavedProject"]
+        XCTAssertTrue(saveButton.exists)
+        saveButton.click()
+        
+        wait = app.waitForExistence(timeout: TimeInterval(1))
+        
+        let addNewProjectButton = app.buttons["AddNewProjectButton"]
+        XCTAssertTrue(addNewProjectButton.exists)
+
+    }
+    
+    func testBackButtonForProjectWithoutNameWithProjectSteps() throws {
+        let newStepButton = app.buttons["NewStepButton"]
+        XCTAssertTrue(newStepButton.exists)
+        newStepButton.click()
+        
+        let newStepTextField = app.textFields["NewStepTextField"]
+        XCTAssertTrue(newStepTextField.exists)
+        
+        newStepTextField.click()
+        newStepTextField.typeText("step 1")
+        
+        let addStepButton = app.buttons["AddNewStepButton"]
+        addStepButton.click()
+        
+        // TODO: figure out why I need to search for the back button twice, first in find toolbar buttons then the button I want in the toolbar
+        let backButton = app.toolbars.buttons["ProjectViewCustomBackButton"].buttons["ProjectViewCustomBackButton"]
+        XCTAssertTrue(backButton.exists)
+        backButton.click()
+        
+        var wait = app.waitForExistence(timeout: TimeInterval(1))
+
+        let alertTextField = app.sheets["alert"].textFields["Enter a project name"]
+        XCTAssertTrue(alertTextField.exists)
+        alertTextField.click()
+        alertTextField.typeText("project 1")
+        
+        let saveButton = app.buttons["SaveButtonInAlertUnsavedProject"]
+        XCTAssertTrue(saveButton.exists)
+        saveButton.click()
+        
+        wait = app.waitForExistence(timeout: TimeInterval(1))
+        
+        let addNewProjectButton = app.buttons["AddNewProjectButton"]
+        XCTAssertTrue(addNewProjectButton.exists)
+    }
+    
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
