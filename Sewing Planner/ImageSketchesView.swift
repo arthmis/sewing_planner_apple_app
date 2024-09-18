3de208a80ee628789734331fe767586b149e995c
Lloyd Massiah <artmis9@protonmail.com>
2024-09-18 17:32:58 -0400

    adding functionality to long press an image and open a menu to delete it

--- Sewing Planner/ImageSketchesView.swift
+++ Sewing Planner/ImageSketchesView.swift
@@ -42,6 +42,7 @@ struct ImageSketchesView: View {
     @State var text = ""
     @State var showFileImporter = false
     @State var projectImages: [SketchData] = []
+    @State var selectedImage: URL?
     
     func deduplicateSelectedImages(images: [SketchData]) -> [SketchData] {
         var result: [SketchData] = []
@@ -59,54 +60,89 @@ struct ImageSketchesView: View {
     
     var body: some View {
         VStack(alignment: .center) {
-            Button {
-                showFileImporter = true
-            } label: {
-                Label("Select image", systemImage: "doc.circle")
-            }.fileImporter(isPresented: $showFileImporter, allowedContentTypes: [.jpeg, .png, .webP, .heic, .heif], allowsMultipleSelection: true) { result in
-                switch result {
-                case .success(let files):
-                    let images: [SketchData] = files.map { file in
-                        let name = file.lastPathComponent
-                        let path = file
-                        
-                        // need this to access the file content
-                        let hasAccess = file.startAccessingSecurityScopedResource()
-                        if !hasAccess {
-                            // must relinquish access once it isn't needed
-                            // hence needing a call at every return point
+            HStack {
+                
+                Button {
+                    showFileImporter = true
+                } label: {
+                    Label("Select image", systemImage: "doc.circle")
+                }.fileImporter(isPresented: $showFileImporter, allowedContentTypes: [.jpeg, .png, .webP, .heic, .heif], allowsMultipleSelection: true) { result in
+                    switch result {
+                    case .success(let files):
+                        let images: [SketchData] = files.map { file in
+                            let name = file.lastPathComponent
+                            let path = file
+                            
+                            // need this to access the file content
+                            let hasAccess = file.startAccessingSecurityScopedResource()
+                            if !hasAccess {
+                                // must relinquish access once it isn't needed
+                                // hence needing a call at every return point
+                                file.stopAccessingSecurityScopedResource()
+                                return SketchData(name: name, path: path)
+                            }
+                            
+                            let data = try! Data(contentsOf: path)
+                            if let img = NSImage(data: data) {
+                                file.stopAccessingSecurityScopedResource()
+                                return SketchData(name: name, path: path, image: img)
+                            }
+                            
                             file.stopAccessingSecurityScopedResource()
                             return SketchData(name: name, path: path)
                         }
-                        
-                        let data = try! Data(contentsOf: path)
-                        if let img = NSImage(data: data) {
-                            file.stopAccessingSecurityScopedResource()
-                            return SketchData(name: name, path: path, image: img)
-                        }
-                        
-                        file.stopAccessingSecurityScopedResource()
-                        return SketchData(name: name, path: path)
+                        print(images)
+                        projectImages += images
+                        projectImages = deduplicateSelectedImages(images: projectImages)
+                    case .failure(let error):
+                        // Process error here
+                        print(error)
                     }
-                    print(images)
-                    projectImages += images
-                    projectImages = deduplicateSelectedImages(images: projectImages)
-                case .failure(let error):
-                    // Process error here
-                    print(error)
+                }
+                if let imagePath = selectedImage {
+                    HStack(alignment: .center) {
+                        Button("Cancel") {
+                            selectedImage = nil
+                        }
+                        Spacer()
+                        Button("Delete") {
+                            self.projectImages = self.projectImages.filter { $0.path != selectedImage!}
+                            selectedImage = nil
+                        }
+                    }
                 }
             }
             ScrollView {
                 LazyVGrid(columns: [GridItem(.adaptive(minimum: 150, maximum: 150))]) {
                     ForEach($projectImages, id: \.self.path) { $image in
                         if let img = image.image {
-                            VStack {
-                                Image(nsImage: img)
-                                    .resizable()
-                                    .interpolation(.high)
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 120, height: 120, alignment: .center)
-                                Text(image.name)
+                            if image.path == selectedImage {
+                                VStack {
+                                    Image(nsImage: img)
+                                        .resizable()
+                                        .interpolation(.high)
+                                        .scaledToFit()
+                                        .frame(width: 120, height: 120, alignment: .center)
+                                    Text(image.name)
+                                }.background(Color.blue)
+                            } else {
+                                VStack {
+                                    Image(nsImage: img)
+                                        .resizable()
+                                        .interpolation(.high)
+                                        .scaledToFit()
+                                        .frame(width: 120, height: 120, alignment: .center)
+                                    Text(image.name)
+                                }.onTapGesture {
+                                    print("clicking image")
+                                    print("enlarging image now..")
+                                    print(image.path)
+                                    
+                                }.onLongPressGesture {
+                                    print("long clicking image")
+                                    print(image.path)
+                                    selectedImage = image.path
+                                }
                             }
                         } else {
                             // TODO: put a placeholder image if image displaying or loading fails
