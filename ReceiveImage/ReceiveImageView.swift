d2c1a75a5a7227b0566ee86c3389eb59fa0c38cf
Lloyd Massiah <artmis9@protonmail.com>
2025-10-24 23:10:54 -0400

    create initial share extension to receive image from sharing

---
+++ ReceiveImage/ReceiveImageView.swift
@@ -0,0 +1,19 @@
+//
+//  ReceiveImageView.swift
+//  ReceiveImage
+//
+//  Created by Art on 10/24/25.
+//
+
+import SwiftUI
+
+struct ReceiveImageView: View {
+    var text: String
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReceiveImageView(text: "hello")
+}
---
+++ ReceiveImage/ShareViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ShareViewController.swift
+//  ReceiveImage
+//
+//  Created by Art on 10/24/25.
+//
+
+import Social
+import SwiftUI
+import UIKit
+import UniformTypeIdentifiers
+
+class ShareViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Ensure access to extensionItem and itemProvider
+        guard
+            let extensionItem = extensionContext?.inputItems.first as? NSExtensionItem,
+            let itemProvider = extensionItem.attachments?.first
+        else {
+            // TODO: do some error handling
+            return
+        }
+        // Check type identifier
+        let imageDataType = UTType.image.identifier
+        if itemProvider.hasItemConformingToTypeIdentifier(imageDataType) {
+            // Load the item from itemProvider
+            itemProvider.loadItem(forTypeIdentifier: imageDataType, options: nil) { providedImage, error in
+                if let error = error {
+                    return
+                }
+
+                if let image = providedImage as? UIImage {
+                    DispatchQueue.main.async {
+                        // host the SwiftU view
+                        let text = "hello"
+                        let contentView = UIHostingController(rootView: ReceiveImageView(text: text))
+                        self.addChild(contentView)
+                        self.view.addSubview(contentView.view)
+
+                        // set up constraints
+                        contentView.view.translatesAutoresizingMaskIntoConstraints = false
+                        contentView.view.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+                        contentView.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+                        contentView.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+                        contentView.view.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+                    }
+                }
+            }
+        } else {
+            close()
+            return
+        }
+    }
+
+    /// Close the Share Extension
+    func close() {
+        extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+    }
+}
