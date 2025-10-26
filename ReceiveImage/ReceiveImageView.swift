01c291b59d8c515a13367d402b9df252fc8d38be
Lloyd Massiah <artmis9@protonmail.com>
2025-10-26 11:54:04 -0400

    add logic to get and display projects in share extension from a shared json file

--- ReceiveImage/ReceiveImageView.swift
+++ ReceiveImage/ReceiveImageView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ReceiveImageView: View {
     let image: UIImage
     @State var projects: [Project] = []
-    @State var selection: Int64 = 0
+    @State var selection: Int64? = 0
 
     var body: some View {
         VStack {
@@ -24,53 +24,48 @@ struct ReceiveImageView: View {
                 .resizable()
                 .aspectRatio(contentMode: .fit)
 
-            Button("Save To Project") {}
+            Button("Save to selected project") {
+                do {
+                    // safe to unwrap because button can't be tapped if there is no selection
+                    try saveImageToProject(projectId: selection!, image: image)
+                } catch {
+                    // TODO: handle error
+                }
+            }
+            .disabled(selection == nil)
         }
         .task {
-            projects = try! SharedPersistence().getProjects()
-            selection = projects[0].id
+            do {
+                projects = try getProjects()
+                selection = projects[0].id
+            } catch {
+                // TODO: show some kind of error and offer retry or something
+            }
         }
     }
 }
 
-struct Project: Identifiable {
-    let id: Int64
-    let name: String
+func getProjects() throws -> [Project] {
+    guard let data = try? SharedPersistence().getFile(fileName: "projects")
+    else {
+        // throw an error
+        throw ShareError.getFile(
+            "Couldn't load projects. Head to the main app and create a project"
+        )
+    }
+    let decoder = JSONDecoder()
+    guard let projects = try? decoder.decode([Project].self, from: data) else {
+        // throw an error
+        //        throw Error
+        throw ShareError.emptyFile(
+            "Couldn't load projects. Head to the main app and create a project"
+        )
+    }
+
+    return projects
 }
 
-// TODO: write the image file out to the app group with some json to describe where
-// it can be found
-// 1. also update add projects logic in main project to output a list of all the projects
-// and when removing all the projects
-// the extension will use this list to populate the view for project selection
-// 2. update logic on project selection to read the shared container and get any image
-// shared to the project if any
-// then delete the image if importing it from the shared container is successful
-struct SharedPersistence {
-    func getProjects() throws -> [Project] {
-        return []
-    }
-
-    func getImagesDirectory() -> URL {
-        let container = getPersistenceLocation()
-        return container!
-    }
-
-    func getPersistenceLocation() -> URL? {
-        // get the shared container for the app group
-        guard
-            let fileContainer = FileManager.default.containerURL(
-                forSecurityApplicationGroupIdentifier: "group.SewingPlanner"
-            )
-        else {
-            fatalError("Shared file container could not be created.")
-        }
-        print(fileContainer)
-        //                return fileContainer.appendingPathComponent("\(databaseName).sqlite")
-
-        return fileContainer
-    }
-}
+func saveImageToProject(projectId _: Int64, image _: UIImage) throws {}
 
 #Preview {
     ReceiveImageView(image: UIImage(named: "black_dress_sketch")!)
---
+++ SharedExtensionFiles/Persistence.swift
@@ -0,0 +1,62 @@
+//
+//  Persistence.swift
+//  Sewing Planner
+//
+//  Created by Art on 10/26/25.
+//
+
+import Foundation
+
+struct Project: Identifiable, Codable {
+    let id: Int64
+    let name: String
+}
+
+enum ShareError: Error {
+    case getFile(String)
+    case emptyFile(String)
+}
+
+// TODO: write the image file out to the app group with some json to describe where
+// it can be found
+// 1. also update add projects logic in main project to output a list of all the projects
+// and when removing all the projects
+// the extension will use this list to populate the view for project selection
+// 2. update logic on project selection to read the shared container and get any image
+// shared to the project if any
+// then delete the image if importing it from the shared container is successful
+struct SharedPersistence {
+    func getFile(fileName: String) throws -> Data? {
+        let fileManager = FileManager.default
+        let sharedLocation = try getPersistenceLocation()!
+        let fileUrl = constructFileLocation(location: sharedLocation, fileName: fileName)
+        let data = try? Data(contentsOf: fileUrl)
+        return data
+    }
+
+//    func getImagesDirectory() -> URL {
+//        let container = getPersistenceLocation()
+//        return container!
+//    }
+
+    func constructFileLocation(location: URL, fileName: String) -> URL {
+        let fileLocation = location.appending(path: fileName).appendingPathExtension(for: .json)
+
+        return fileLocation
+    }
+
+    func getPersistenceLocation() throws -> URL? {
+        // get the shared container for the app group
+        guard
+            let fileContainer = FileManager.default.containerURL(
+                forSecurityApplicationGroupIdentifier: "group.SewingPlanner"
+            )
+        else {
+            fatalError("Shared file container could not be created.")
+        }
+        print(fileContainer)
+        //                return fileContainer.appendingPathComponent("\(databaseName).sqlite")
+
+        return fileContainer
+    }
+}
