1375b4c2c864e9e07e5b53373fae9fb5cee1d85e
Lloyd Massiah <artmis9@protonmail.com>
2025-11-17 16:28:43 -0500

    add blank slate call to action view when project doesn't have any sections
    - some formatting changes happened as well

--- Sewing Planner/Src/Project/Details/Views/ProjectDetailsView.swift
+++ Sewing Planner/Src/Project/Details/Views/ProjectDetailsView.swift
@@ -8,61 +8,99 @@
 import SwiftUI
 
 struct ProjectDataView: View {
+  @Environment(ProjectViewModel.self) var project
+  @Environment(\.appDatabase) var db
+
+  var body: some View {
+    @Bindable var projectBinding = project
+    VStack(alignment: .leading) {
+      HStack {
+        ProjectTitle(
+          projectData: $projectBinding.projectData.data,
+          bindedName: $projectBinding.projectData.bindedName,
+          updateName: project.projectData.updateName
+        )
+        Spacer()
+      }
+      .frame(maxWidth: .infinity)
+      .padding(.bottom, 25)
+      if project.projectData.sections.isEmpty {
+        EmptyProjectCallToActionView()
+        Spacer()
+      } else {
+        ScrollView {
+          VStack(alignment: .leading) {
+            ForEach($projectBinding.projectData.sections, id: \.id) {
+              $section in
+              SectionView(model: $section)
+                .padding(.bottom, 16)
+            }
+          }
+        }
+        .frame(maxHeight: .infinity)
+      }
+    }
+    .padding([.leading, .trailing], 8)
+    .confirmationDialog(
+      "Delete Section",
+      isPresented: $projectBinding.projectData.showDeleteSectionDialog
+    ) {
+      Button("Delete", role: .destructive) {
+        Task {
+          await project.handleEffect(
+            effect: project.deleteSection(
+              selectedSection: project.projectData
+                .selectedSectionForDeletion
+            ),
+            db: db
+          )
+        }
+      }
+      Button("Cancel", role: .cancel) {
+        project.projectData.cancelDeleteSection()
+      }
+    } message: {
+      if let section = project.projectData.selectedSectionForDeletion {
+        Text("Delete \(section.name)")
+      }
+    }
+  }
+
+  struct EmptyProjectCallToActionView: View {
     @Environment(ProjectViewModel.self) var project
-    @Environment(\.appDatabase) var db
 
     var body: some View {
-        @Bindable var projectBinding = project
-        VStack(alignment: .leading) {
-            HStack {
-                ProjectTitle(
-                    projectData: $projectBinding.projectData.data,
-                    bindedName: $projectBinding.projectData.bindedName,
-                    updateName: project.projectData.updateName
-                )
-                Spacer()
-            }
-            .frame(maxWidth: .infinity)
-            .padding(.bottom, 25)
-            ScrollView {
-                VStack(alignment: .leading) {
-                    ForEach($projectBinding.projectData.sections, id: \.id) {
-                        $section in
-                        SectionView(model: $section)
-                            .padding(.bottom, 16)
-                    }
-                }
-            }
-            .frame(maxHeight: .infinity)
+        VStack(alignment: .leading, spacing: 0) {
+        Image(systemName: "list.bullet.rectangle")
+          .font(.system(size: 32, weight: .light))
+        Text("Sections")
+          .font(.system(size: 20, weight: .semibold))
+          .padding(.top, 20)
+
+        Text(
+          "Create sections to organize and define the important tasks and details of your project."
+        )
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .font(.system(size: 16))
+        .padding(.top, 8)
+        Button("Create new section") {
+          project.addSection()
         }
-        .padding([.leading, .trailing], 8)
-        .confirmationDialog(
-            "Delete Section",
-            isPresented: $projectBinding.projectData.showDeleteSectionDialog
-        ) {
-            Button("Delete", role: .destructive) {
-                Task {
-                    await project.handleEffect(
-                        effect: project.deleteSection(
-                            selectedSection: project.projectData
-                                .selectedSectionForDeletion
-                        ),
-                        db: db
-                    )
-                }
-            }
-            Button("Cancel", role: .cancel) {
-                project.projectData.cancelDeleteSection()
-            }
-        } message: {
-            if let section = project.projectData.selectedSectionForDeletion {
-                Text("Delete \(section.name)")
-            }
-        }
+        .buttonStyle(PrimaryButtonStyle(fontSize: 16))
+        .padding(.top, 28)
+      }
     }
+
+  }
 }
 
-// #Preview {
-//    ProjectDetails(project: ProjectData(
-//    data: Project(id: 2, name: "Project Name", completed: false, createDate: Date(), updateDate: Date())), projectSections: ProjectSections(), projectsNavigation: [Project()])
-// }
+#Preview {
+  let viewModel = ProjectViewModel(
+    data: ProjectData(
+      data: ProjectMetadata(
+        id: 1, name: "Project Name", completed: false, createDate: Date(), updateDate: Date())),
+    projectsNavigation: [], projectImages: ProjectImages(projectId: 1))
+  ProjectDataView()
+    .environment(viewModel)
+
+}
--- Sewing Planner/Src/Styling/AppButtonStyles.swift
+++ Sewing Planner/Src/Styling/AppButtonStyles.swift
@@ -9,96 +9,102 @@ import Foundation
 import SwiftUI
 
 struct PrimaryButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        let isPressed = configuration.isPressed
-        configuration.label
-            .font(.custom("CooperHewitt-Regular", size: 18))
-            .padding([.top, .bottom], 12)
-            .padding([.leading, .trailing], 16)
-            .background(Color(hex: 0x131944, opacity: 1.0))
-            .foregroundColor(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 12))
-            .scaleEffect(isPressed ? 0.95 : 1)
-            .brightness(isPressed ? -0.05 : 0)
-            .shadow(color: Color(hex: 0x000000, opacity: 0.25), radius: isPressed ? 1.5 : 3, x: 0, y: 4)
-            .animation(.easeIn(duration: 0.1), value: isPressed)
-    }
+  let fontSize: CGFloat
+
+  init(fontSize: CGFloat = 18) {
+    self.fontSize = fontSize
+  }
+
+  func makeBody(configuration: Configuration) -> some View {
+    let isPressed = configuration.isPressed
+    configuration.label
+      .font(.custom("CooperHewitt-Regular", size: fontSize))
+      .padding([.top, .bottom], 12)
+      .padding([.leading, .trailing], 16)
+      .background(Color(hex: 0x131944, opacity: 1.0))
+      .foregroundColor(.white)
+      .clipShape(RoundedRectangle(cornerRadius: 12))
+      .scaleEffect(isPressed ? 0.95 : 1)
+      .brightness(isPressed ? -0.05 : 0)
+      .shadow(color: Color(hex: 0x000000, opacity: 0.25), radius: isPressed ? 1.5 : 3, x: 0, y: 4)
+      .animation(.easeIn(duration: 0.1), value: isPressed)
+  }
 }
 
 struct DeleteButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        let isPressed = configuration.isPressed
-        configuration.label
-//            .font(.custom("CooperHewitt-Regular", size: 16))
-//            .font(.custom("CooperHewitt-Regular", size: 16))
-            .padding(8)
-            .background(Color(hex: 0xF85A3E, opacity: 1.0))
-            .foregroundStyle(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 4))
-            .scaleEffect(isPressed ? 0.95 : 1)
-            .brightness(isPressed ? -0.05 : 0)
-//            .shadow(color: Color(hex: 0x000000, opacity: 0.25), radius: isPressed ? 1.5 : 3, x: 0, y: 4)
-            .animation(.easeIn(duration: 0.1), value: isPressed)
-    }
+  func makeBody(configuration: Configuration) -> some View {
+    let isPressed = configuration.isPressed
+    configuration.label
+      //            .font(.custom("CooperHewitt-Regular", size: 16))
+      //            .font(.custom("CooperHewitt-Regular", size: 16))
+      .padding(8)
+      .background(Color(hex: 0xF85A3E, opacity: 1.0))
+      .foregroundStyle(.white)
+      .clipShape(RoundedRectangle(cornerRadius: 4))
+      .scaleEffect(isPressed ? 0.95 : 1)
+      .brightness(isPressed ? -0.05 : 0)
+      //            .shadow(color: Color(hex: 0x000000, opacity: 0.25), radius: isPressed ? 1.5 : 3, x: 0, y: 4)
+      .animation(.easeIn(duration: 0.1), value: isPressed)
+  }
 }
 
 struct SaveProjectButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        let isPressed = configuration.isPressed
-        configuration.label
-            .padding([.top, .bottom], 7.5)
-            .padding([.leading, .trailing], 18)
-            .background(Color(hex: 0x131944, opacity: 1.0))
-            .foregroundColor(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 9))
-            .scaleEffect(isPressed ? 0.95 : 1)
-            .brightness(isPressed ? -0.05 : 0)
-            .shadow(color: Color(hex: 0x000000, opacity: 0.25), radius: isPressed ? 1.5 : 3, x: 0, y: 4)
-            .animation(.easeIn(duration: 0.1), value: isPressed)
-    }
+  func makeBody(configuration: Configuration) -> some View {
+    let isPressed = configuration.isPressed
+    configuration.label
+      .padding([.top, .bottom], 7.5)
+      .padding([.leading, .trailing], 18)
+      .background(Color(hex: 0x131944, opacity: 1.0))
+      .foregroundColor(.white)
+      .clipShape(RoundedRectangle(cornerRadius: 9))
+      .scaleEffect(isPressed ? 0.95 : 1)
+      .brightness(isPressed ? -0.05 : 0)
+      .shadow(color: Color(hex: 0x000000, opacity: 0.25), radius: isPressed ? 1.5 : 3, x: 0, y: 4)
+      .animation(.easeIn(duration: 0.1), value: isPressed)
+  }
 }
 
 struct AddNewSectionButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        let isPressed = configuration.isPressed
-        configuration.label
-            .font(.custom("CooperHewitt-Regular", size: 16))
-            .padding(8)
-            .background(Color(hex: 0x131944, opacity: 1.0))
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(isPressed ? 0.95 : 1)
-            .brightness(isPressed ? -0.05 : 0)
-            .shadow(color: Color(hex: 0x000000, opacity: 0.25), radius: isPressed ? 1.5 : 3, x: 0, y: 4)
-            .animation(.easeIn(duration: 0.1), value: isPressed)
-    }
+  func makeBody(configuration: Configuration) -> some View {
+    let isPressed = configuration.isPressed
+    configuration.label
+      .font(.custom("CooperHewitt-Regular", size: 16))
+      .padding(8)
+      .background(Color(hex: 0x131944, opacity: 1.0))
+      .foregroundColor(.white)
+      .clipShape(Circle())
+      .scaleEffect(isPressed ? 0.95 : 1)
+      .brightness(isPressed ? -0.05 : 0)
+      .shadow(color: Color(hex: 0x000000, opacity: 0.25), radius: isPressed ? 1.5 : 3, x: 0, y: 4)
+      .animation(.easeIn(duration: 0.1), value: isPressed)
+  }
 }
 
 struct SectionTertiaryButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        let isPressed = configuration.isPressed
-        configuration.label
-            .font(.system(size: 22))
-            .padding([.vertical, .leading], 10)
-            .foregroundStyle(isPressed ? Color(.blue) : Color(hex: 0x333333))
-            .scaleEffect(isPressed ? 0.95 : 1)
-            .brightness(isPressed ? -0.05 : 0)
-            .background(.white)
-            .animation(.easeIn(duration: 0.1), value: isPressed)
-    }
+  func makeBody(configuration: Configuration) -> some View {
+    let isPressed = configuration.isPressed
+    configuration.label
+      .font(.system(size: 22))
+      .padding([.vertical, .leading], 10)
+      .foregroundStyle(isPressed ? Color(.blue) : Color(hex: 0x333333))
+      .scaleEffect(isPressed ? 0.95 : 1)
+      .brightness(isPressed ? -0.05 : 0)
+      .background(.white)
+      .animation(.easeIn(duration: 0.1), value: isPressed)
+  }
 }
 
 struct AddImageButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        let isPressed = configuration.isPressed
-        configuration.label
-            .foregroundColor(Color(hex: 0x131944, opacity: 1.0))
-            .font(.system(size: 24))
-            .padding(8)
-            .clipShape(RoundedRectangle(cornerRadius: 9))
-            .scaleEffect(isPressed ? 0.95 : 1)
-            .brightness(isPressed ? -0.05 : 0)
-            .shadow(color: Color(hex: 0x000000, opacity: 0.25), radius: isPressed ? 1.5 : 3, x: 0, y: 4)
-            .animation(.easeIn(duration: 0.1), value: isPressed)
-    }
+  func makeBody(configuration: Configuration) -> some View {
+    let isPressed = configuration.isPressed
+    configuration.label
+      .foregroundColor(Color(hex: 0x131944, opacity: 1.0))
+      .font(.system(size: 24))
+      .padding(8)
+      .clipShape(RoundedRectangle(cornerRadius: 9))
+      .scaleEffect(isPressed ? 0.95 : 1)
+      .brightness(isPressed ? -0.05 : 0)
+      .shadow(color: Color(hex: 0x000000, opacity: 0.25), radius: isPressed ? 1.5 : 3, x: 0, y: 4)
+      .animation(.easeIn(duration: 0.1), value: isPressed)
+  }
 }
