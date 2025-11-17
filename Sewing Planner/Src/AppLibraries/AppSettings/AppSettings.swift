b276c8bee21e707cb571435ce24d120f9eea5ebe
Lloyd Massiah <artmis9@protonmail.com>
2025-11-17 13:39:08 -0500

    move AppSettings into AppLibraries

---
+++ Sewing Planner/Src/AppLibraries/AppSettings/AppSettings.swift
@@ -0,0 +1,139 @@
+import Foundation
+// import os
+import Logging
+import UniformTypeIdentifiers
+
+protocol Settings {
+  func set<T: Codable>(_ value: T, forKey: String) throws
+  func get<T: Codable>(forKey key: String) throws -> T?
+}
+
+class AppSettings {
+  private let directoryName: String
+  private var data: [String: Data]
+  private let settingsFileManager: any AppSettingsFileManagerProtocol
+  private let logger: AppLogger
+
+  init(
+    directoryName: String, data: [String: Data]? = nil,
+    settingsFileManager: (any AppSettingsFileManagerProtocol)? = nil,
+    logger: AppLogger
+  ) {
+    self.directoryName = directoryName
+    self.data = data ?? [:]
+    self.logger = AppLogger(label: "com.fabricstash.settings")
+
+    let fileManager = FileManager.default
+    self.settingsFileManager =
+      settingsFileManager
+      ?? AppSettingsFileManager(fileManager: fileManager, directoryName: directoryName)
+
+    do {
+      print("Retrieving app settings")
+      let settingsFileData = try self.settingsFileManager.getSettingsFileData()
+
+      do {
+        let decoder = JSONDecoder()
+        self.data = try decoder.decode([String: Data].self, from: settingsFileData)
+      } catch {
+        let metadata: Logger.Metadata = [
+          "directory name": .string(directoryName), "error": .string(error.localizedDescription),
+        ]
+        self.logger.log(
+          level: .error, message: "Couldn't decode app settings file", metadata: metadata)
+      }
+    } catch {
+      let metadata: Logger.Metadata = [
+        "directory name": .string(directoryName), "error": .string(error.localizedDescription),
+      ]
+      self.logger.log(
+        level: .error, message: "Couldn't get app settings file", metadata: metadata)
+    }
+  }
+}
+
+extension AppSettings: Settings {
+  func set<T: Codable>(_ value: T, forKey key: String) throws {
+    let encoder = JSONEncoder()
+    let data = try encoder.encode(value)
+
+    _ = self.data.updateValue(data, forKey: key)
+    let dataToPersist = try encoder.encode(self.data)
+
+    try settingsFileManager.writeSettings(dataToPersist)
+  }
+
+  func get<T: Codable>(forKey key: String) throws -> T? {
+    let data = self.data[key]
+    let decoder = JSONDecoder()
+    if let setting = data {
+      return try decoder.decode(T.self, from: setting)
+    }
+
+    let settingsFileData = try settingsFileManager.getSettingsFileData()
+    let settings = try decoder.decode([String: Data].self, from: settingsFileData)
+    guard let settingData = settings[key] else {
+      return nil
+    }
+
+    let setting = try decoder.decode(T.self, from: settingData)
+    self.data.updateValue(settingData, forKey: key)
+
+    return setting
+  }
+}
+
+protocol AppSettingsFileManagerProtocol {
+  func writeSettings(_ data: Data) throws
+  func getSettingsFileData() throws -> Data
+}
+
+private struct AppSettingsFileManager: AppSettingsFileManagerProtocol {
+  private let fileManager: FileManager
+  private let directoryName: String
+  private let logger: AppLogger
+
+  init(fileManager: FileManager, directoryName: String, logger: AppLogger? = nil) {
+    self.fileManager = fileManager
+    self.directoryName = directoryName
+    self.logger = logger ?? AppLogger(label: "com.fabricstash.settings.filemanager")
+  }
+
+  func writeSettings(_ data: Data) throws {
+    let settingsDirectory = getSettingsDirectory(fileManager)
+
+    let settingsFolderExists = fileManager.fileExists(atPath: settingsDirectory.path())
+
+    if !settingsFolderExists {
+      try fileManager.createDirectory(
+        at: settingsDirectory, withIntermediateDirectories: true, attributes: nil)
+    }
+
+    let filePath = settingsDirectory.appendingPathComponent("settings").appendingPathExtension(
+      for: .json)
+
+    try data.write(to: filePath, options: [.atomic, .completeFileProtection])
+  }
+
+  func getSettingsFileData() throws -> Data {
+    let settingsDirectory = getSettingsDirectory(fileManager)
+
+    let settingsFolderExists = fileManager.fileExists(atPath: settingsDirectory.path())
+
+    if !settingsFolderExists {
+      try fileManager.createDirectory(
+        at: settingsDirectory, withIntermediateDirectories: true, attributes: nil)
+    }
+
+    let filePath = settingsDirectory.appendingPathComponent("settings").appendingPathExtension(
+      for: .json)
+
+    return try Data(contentsOf: filePath)
+  }
+
+  private func getSettingsDirectory(_ fileManager: FileManager) -> URL {
+    let documentsUrl = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+
+    return documentsUrl.appendingPathComponent(directoryName)
+  }
+}
---
+++ Sewing Planner/Src/AppLibraries/AppSettings/UserSettings.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+private let CREATED_ONE_PROJECT = "Created Project First Time"
+
+class UserSettings {
+  private let settingsManager: any Settings
+  private var createdOneProject: Bool
+  private let logger: AppLogger
+
+  init(
+    settingsDirectory: String,
+    logger: AppLogger
+  ) {
+    settingsManager = AppSettings(directoryName: settingsDirectory, logger: logger)
+    self.logger = logger
+
+    logger.log(level: .info, message: "instantiating settings manager")
+
+    do {
+      createdOneProject = try settingsManager.get(forKey: CREATED_ONE_PROJECT) ?? false
+    } catch {
+      createdOneProject = false
+    }
+  }
+
+  func userCreatedProjectFirstTime(val: Bool) throws {
+    let createdOneProject = val
+
+    try settingsManager.set(createdOneProject, forKey: CREATED_ONE_PROJECT)
+  }
+
+  func getUserCreatedProjectFirstTime() -> Bool? {
+    do {
+      return try settingsManager.get(forKey: CREATED_ONE_PROJECT)
+    } catch {
+      // TODO: log the error
+      print(error)
+      return nil
+    }
+  }
+}
